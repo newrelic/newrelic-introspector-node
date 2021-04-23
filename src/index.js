2008cc01d9ffb5e83899e876964d6c8c1d1a6ffe
Gabe O'Brien <gabe@newrelic.com>
2021-04-23 11:11:58 -0700

    Updates LSI to support reporing to staging

--- src/index.js
+++ src/index.js
@@ -38,6 +38,10 @@ yargs
       appName: {
         string: true
       }
+      region: {
+        string: true,
+        default: process.env.NEW_RELIC_REGION || 'US'
+       }
     },
     instrument
   )

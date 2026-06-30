3a0f2a7ac404dbd51ef50beaca79fe33b746af49
Bob Evans <robert.evans25@gmail.com>
2026-06-30 12:14:25 -0400

    test: Migrated the tests from `tap` to `node:test`

--- test/unit/commands.test.js
+++ test/unit/commands.test.js
@@ -6,192 +6,195 @@
 /* eslint-disable no-console */
 'use strict'
 
-const tap = require('tap')
+const test = require('node:test')
+const assert = require('node:assert')
 const utils = require('../../src/utils')
 const sinon = require('sinon')
 
-tap.test('commands', (t) => {
-  let sandbox
-  let list
-  let introspect
-  let instrument
+test.beforeEach((ctx) => {
+  const sandbox = sinon.createSandbox()
+  sandbox.stub(console, 'log')
+  sandbox.stub(console, 'error')
+  sandbox.stub(process, 'exit')
+  sandbox.stub(utils, 'pm2List')
+  sandbox.stub(utils, 'getProc')
+  sandbox.stub(utils, 'getTruePath')
+  sandbox.stub(utils, 'pm2RestartandSave')
+  sandbox.stub(utils, 'exec')
+  const { list, introspect, instrument } = require('../../src/commands')
+  ctx.nr = { sandbox, list, introspect, instrument }
+})
 
-  t.autoend()
+test.afterEach((ctx) => {
+  ctx.nr.sandbox.restore()
+  // clean up so stubs do not persist
+  delete require.cache[require.resolve('../../src/commands')]
+})
 
-  t.beforeEach(() => {
-    sandbox = sinon.createSandbox()
-    sandbox.stub(console, 'log')
-    sandbox.stub(console, 'error')
-    sandbox.stub(process, 'exit')
-    sandbox.stub(utils, 'pm2List')
-    sandbox.stub(utils, 'getProc')
-    sandbox.stub(utils, 'getTruePath')
-    sandbox.stub(utils, 'pm2RestartandSave')
-    sandbox.stub(utils, 'exec')
-    ;({ list, introspect, instrument } = require('../../src/commands'))
-  })
-
-  t.afterEach(() => {
-    sandbox.restore()
-    // clean up so stubs do not persist
-    delete require.cache[require.resolve('../../src/commands')]
-  })
-
-  t.test('list should list all pm2 online pids', async (t) => {
-    const processes = [
-      {
-        pid: 0,
-        pm2_env: {
-          status: 'online'
-        }
-      },
-      {
-        pid: 1,
-        pm2_env: {
-          status: 'stopped'
-        }
-      },
-      {
-        pid: 2,
-        pm2_env: {
-          status: 'online'
-        }
+test('list should list all pm2 online pids', async (t) => {
+  const { list } = t.nr
+  const processes = [
+    {
+      pid: 0,
+      pm2_env: {
+        status: 'online'
       }
-    ]
+    },
+    {
+      pid: 1,
+      pm2_env: {
+        status: 'stopped'
+      }
+    },
+    {
+      pid: 2,
+      pm2_env: {
+        status: 'online'
+      }
+    }
+  ]
 
-    utils.pm2List.resolves(processes)
-    await list()
-    t.same(console.log.args[0][0], [0, 2], 'should only list pids online')
-    t.equal(process.exit.callCount, 1, 'should exit once')
-    t.equal(process.exit.args[0][0], 0, 'should called process exit with code 0')
-  })
+  utils.pm2List.resolves(processes)
+  await list()
+  assert.deepEqual(console.log.args[0][0], [0, 2], 'should only list pids online')
+  assert.equal(process.exit.callCount, 1, 'should exit once')
+  assert.equal(process.exit.args[0][0], 0, 'should called process exit with code 0')
+})
 
-  t.test('should list 0 pids if no pm2 processes exist', async (t) => {
-    utils.pm2List.resolves([])
-    await list()
-    t.same(console.log.args[0][0], [], 'should not list any pids')
-    t.equal(process.exit.callCount, 1, 'should exit once')
-    t.equal(process.exit.args[0][0], 0, 'should called process exit with code 0')
-  })
+test('should list 0 pids if no pm2 processes exist', async (t) => {
+  const { list } = t.nr
+  utils.pm2List.resolves([])
+  await list()
+  assert.deepEqual(console.log.args[0][0], [], 'should not list any pids')
+  assert.equal(process.exit.callCount, 1, 'should exit once')
+  assert.equal(process.exit.args[0][0], 0, 'should called process exit with code 0')
+})
 
-  t.test('should log console error when pm2List fails', async (t) => {
-    const err = new Error('failed to list pids')
-    utils.pm2List.rejects(err)
-    await list()
-    t.same(console.error.args[0][0], err, 'should log error')
-    t.equal(process.exit.callCount, 1, 'should exit once')
-    t.equal(process.exit.args[0][0], 1, 'should called process exit with code 1')
-  })
+test('should log console error when pm2List fails', async (t) => {
+  const { list } = t.nr
+  const err = new Error('failed to list pids')
+  utils.pm2List.rejects(err)
+  await list()
+  assert.deepEqual(console.error.args[0][0], err, 'should log error')
+  assert.equal(process.exit.callCount, 1, 'should exit once')
+  assert.equal(process.exit.args[0][0], 1, 'should called process exit with code 1')
+})
 
-  t.test('introspect should list pm2 metadata in json format', async (t) => {
-    const pm2Meta = {
+test('introspect should list pm2 metadata in json format', async (t) => {
+  const { introspect } = t.nr
+  const pm2Meta = {
+    pid: 0,
+    pm_id: 1,
+    name: 'Unit Test',
+    pm2_env: {
+      pm_exec_path: '/path/to/pm2',
+      pm_cwd: 'pm2 list',
+      node_version: '16.20.2'
+    }
+  }
+
+  utils.getProc.resolves(pm2Meta)
+
+  await introspect({ pid: 0 })
+  assert.equal(
+    console.log.args[0][0],
+    JSON.stringify({
       pid: 0,
       pm_id: 1,
       name: 'Unit Test',
-      pm2_env: {
-        pm_exec_path: '/path/to/pm2',
-        pm_cwd: 'pm2 list',
-        node_version: '16.20.2'
-      }
-    }
-
-    utils.getProc.resolves(pm2Meta)
-
-    await introspect({ pid: 0 })
-    t.equal(
-      console.log.args[0][0],
-      JSON.stringify({
-        pid: 0,
-        pm_id: 1,
-        name: 'Unit Test',
-        pm_exec_path: '/path/to/pm2',
-        pm_cwd: 'pm2 list',
-        node_version: '16.20.2'
-      }),
-      'should list meta about a pid'
-    )
-    t.equal(process.exit.callCount, 1, 'should exit once')
-    t.equal(process.exit.args[0][0], 0, 'should called process exit with code 0')
-  })
-
-  t.test('introspect should log error when getProc fails', async (t) => {
-    const err = new Error('failed to get pid meta')
-    utils.getProc.rejects(err)
-    await introspect({})
-    t.same(console.error.args[0][0], err, 'should log error')
-    t.equal(process.exit.callCount, 1, 'should exit once')
-    t.equal(process.exit.args[0][0], 1, 'should called process exit with code 1')
-  })
-
-  t.test('instrument should install agent and restart pm2 with agent loaded', async (t) => {
-    const pidMeta = { pm_id: 0 }
-    utils.getProc.resolves(pidMeta)
-    const truePath = '/path/for/realz'
-    utils.getTruePath.resolves(truePath)
-    utils.pm2RestartandSave.resolves()
-    const output = {
-      stderr: 'no error',
-      stdout: 'install done!'
-    }
-    utils.exec.resolves(output)
-
-    await instrument({ pid: 0 })
-    t.same(console.error.callCount, 3, 'should call console.error three times')
-    t.equal(console.error.args[0][0], output.stdout, 'should log stdout')
-    t.equal(console.error.args[1][0], output.stderr, 'should log stderr')
-    t.equal(console.error.args[2][0], 'Process instrumented successfully')
-    t.equal(utils.exec.args[0][1], truePath, 'should install with truePath')
-    t.equal(utils.pm2RestartandSave.args[0][0], 0, 'should call restart with the appropriate pid')
-    t.equal(process.exit.callCount, 1, 'should exit once')
-    t.equal(process.exit.args[0][0], 0, 'should called process exit with code 0')
-  })
-
-  t.test('should set process env vars from argv', async (t) => {
-    utils.getProc.resolves({})
-    utils.getTruePath.resolves()
-    utils.exec.resolves({})
-    utils.pm2RestartandSave.resolves()
-
-    const argv = {
-      pid: 0,
-      licenseKey: 'super-secret',
-      appName: 'my-app',
-      region: 'prod'
-    }
-
-    await instrument(argv)
-    t.equal(process.env.NEW_RELIC_LICENSE_KEY, argv.licenseKey)
-    t.equal(process.env.NEW_RELIC_APP_NAME, argv.appName)
-    t.equal(process.env.NEW_RELIC_HOST, undefined)
-    t.equal(process.env.NEW_RELIC_DISTRIBUTED_TRACING_ENABLED, 'true')
-  })
-
-  t.test('should set NEW_RELIC_HOST to staging url if region is staging', async (t) => {
-    utils.getProc.resolves({ name: 'test-app' })
-    utils.getTruePath.resolves()
-    utils.exec.resolves({})
-    utils.pm2RestartandSave.resolves()
-
-    const argv = {
-      pid: 0,
-      licenseKey: 'super-secret',
-      region: 'staging'
-    }
-
-    await instrument(argv)
-    t.equal(process.env.NEW_RELIC_HOST, 'staging-collector.newrelic.com')
-    t.equal(process.env.NEW_RELIC_APP_NAME, 'test-app')
-  })
-
-  t.test(
-    'should log error and exit with code 0 if any async function fails in instrument',
-    async (t) => {
-      const err = new Error('failed to get pid meta')
-      utils.getProc.rejects(err)
-      await instrument({})
-      t.same(console.error.args[0][0], err, 'should log error')
-      t.equal(process.exit.callCount, 1, 'should exit once')
-      t.equal(process.exit.args[0][0], 1, 'should called process exit with code 1')
-    }
+      pm_exec_path: '/path/to/pm2',
+      pm_cwd: 'pm2 list',
+      node_version: '16.20.2'
+    }),
+    'should list meta about a pid'
   )
+  assert.equal(process.exit.callCount, 1, 'should exit once')
+  assert.equal(process.exit.args[0][0], 0, 'should called process exit with code 0')
+})
+
+test('introspect should log error when getProc fails', async (t) => {
+  const { introspect } = t.nr
+  const err = new Error('failed to get pid meta')
+  utils.getProc.rejects(err)
+  await introspect({})
+  assert.deepEqual(console.error.args[0][0], err, 'should log error')
+  assert.equal(process.exit.callCount, 1, 'should exit once')
+  assert.equal(process.exit.args[0][0], 1, 'should called process exit with code 1')
+})
+
+test('instrument should install agent and restart pm2 with agent loaded', async (t) => {
+  const { instrument } = t.nr
+  const pidMeta = { pm_id: 0 }
+  utils.getProc.resolves(pidMeta)
+  const truePath = '/path/for/realz'
+  utils.getTruePath.resolves(truePath)
+  utils.pm2RestartandSave.resolves()
+  const output = {
+    stderr: 'no error',
+    stdout: 'install done!'
+  }
+  utils.exec.resolves(output)
+
+  await instrument({ pid: 0 })
+  assert.deepEqual(console.error.callCount, 3, 'should call console.error three times')
+  assert.equal(console.error.args[0][0], output.stdout, 'should log stdout')
+  assert.equal(console.error.args[1][0], output.stderr, 'should log stderr')
+  assert.equal(console.error.args[2][0], 'Process instrumented successfully')
+  assert.equal(utils.exec.args[0][1], truePath, 'should install with truePath')
+  assert.equal(
+    utils.pm2RestartandSave.args[0][0],
+    0,
+    'should call restart with the appropriate pid'
+  )
+  assert.equal(process.exit.callCount, 1, 'should exit once')
+  assert.equal(process.exit.args[0][0], 0, 'should called process exit with code 0')
+})
+
+test('should set process env vars from argv', async (t) => {
+  const { instrument } = t.nr
+  utils.getProc.resolves({})
+  utils.getTruePath.resolves()
+  utils.exec.resolves({})
+  utils.pm2RestartandSave.resolves()
+
+  const argv = {
+    pid: 0,
+    licenseKey: 'super-secret',
+    appName: 'my-app',
+    region: 'prod'
+  }
+
+  await instrument(argv)
+  assert.equal(process.env.NEW_RELIC_LICENSE_KEY, argv.licenseKey)
+  assert.equal(process.env.NEW_RELIC_APP_NAME, argv.appName)
+  assert.equal(process.env.NEW_RELIC_HOST, undefined)
+  assert.equal(process.env.NEW_RELIC_DISTRIBUTED_TRACING_ENABLED, 'true')
+})
+
+test('should set NEW_RELIC_HOST to staging url if region is staging', async (t) => {
+  const { instrument } = t.nr
+  utils.getProc.resolves({ name: 'test-app' })
+  utils.getTruePath.resolves()
+  utils.exec.resolves({})
+  utils.pm2RestartandSave.resolves()
+
+  const argv = {
+    pid: 0,
+    licenseKey: 'super-secret',
+    region: 'staging'
+  }
+
+  await instrument(argv)
+  assert.equal(process.env.NEW_RELIC_HOST, 'staging-collector.newrelic.com')
+  assert.equal(process.env.NEW_RELIC_APP_NAME, 'test-app')
+})
+
+test('should log error and exit with code 0 if any async function fails in instrument', async (t) => {
+  const { instrument } = t.nr
+  const err = new Error('failed to get pid meta')
+  utils.getProc.rejects(err)
+  await instrument({})
+  assert.deepEqual(console.error.args[0][0], err, 'should log error')
+  assert.equal(process.exit.callCount, 1, 'should exit once')
+  assert.equal(process.exit.args[0][0], 1, 'should called process exit with code 1')
 })
--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -5,86 +5,76 @@
 
 'use strict'
 
-const tap = require('tap')
+const test = require('node:test')
+const assert = require('node:assert')
 const execPromise = require('../../src/execPromise')
 const sinon = require('sinon')
 
 const { getProc, pm2RestartandSave } = require('../../src/utils')
 
-tap.test('utils', (test) => {
-  let execStub
+test.beforeEach((ctx) => {
+  ctx.nr = { execStub: sinon.stub(execPromise, 'execPromise') }
+})
 
-  test.autoend()
+test.afterEach((ctx) => {
+  ctx.nr.execStub.restore()
+})
 
-  test.beforeEach(() => {
-    execStub = sinon.stub(execPromise, 'execPromise')
-  })
-
-  test.afterEach(() => {
-    execStub.restore()
-  })
-
-  test.test('getProc returns running node app info', async (t) => {
-    const stdout = [
-      {
-        pid: 0
-      }
-    ]
-
-    const processObject = {
-      stdout: JSON.stringify(stdout),
-      stderr: ''
+test('getProc returns running node app info', async (t) => {
+  const { execStub } = t.nr
+  const stdout = [
+    {
+      pid: 0
     }
-    execStub.returns(Promise.resolve(processObject))
-    const result = await getProc(0)
+  ]
 
-    t.equal(result.pid, 0)
+  const processObject = {
+    stdout: JSON.stringify(stdout),
+    stderr: ''
+  }
+  execStub.returns(Promise.resolve(processObject))
+  const result = await getProc(0)
 
-    t.end()
-  })
+  assert.equal(result.pid, 0)
+})
 
-  test.test('getProc should throw when pid not found', async (t) => {
-    const stdout = []
+test('getProc should throw when pid not found', async (t) => {
+  const { execStub } = t.nr
+  const stdout = []
 
-    const processObject = {
-      stdout: JSON.stringify(stdout),
-      stderr: ''
-    }
-    execStub.returns(Promise.resolve(processObject))
+  const processObject = {
+    stdout: JSON.stringify(stdout),
+    stderr: ''
+  }
+  execStub.returns(Promise.resolve(processObject))
 
-    try {
-      await getProc(0)
-    } catch (err) {
-      t.ok(err, 'throws when pid not found')
-    }
+  await assert.rejects(getProc(0), 'throws when pid not found')
+})
 
-    t.end()
-  })
+test('pm2RestartAndSave should restart and save in pm2', async (t) => {
+  const { execStub } = t.nr
+  execStub.resolves()
+  await pm2RestartandSave(1)
+  assert.equal(execStub.callCount, 2, 'should call restart and save')
+  assert.equal(
+    execStub.args[0][0],
+    'pm2 restart 1 --update-env --node-args "-r newrelic"',
+    'should call restart with proper args'
+  )
+  assert.equal(execStub.args[1][0], 'pm2 save', 'should call save')
+})
 
-  test.test('pm2RestartAndSave should restart and save in pm2', async (t) => {
-    execStub.resolves()
-    await pm2RestartandSave(1)
-    t.equal(execStub.callCount, 2, 'should call restart and save')
-    t.equal(
-      execStub.args[0][0],
-      'pm2 restart 1 --update-env --node-args "-r newrelic"',
-      'should call restart with proper args'
-    )
-    t.equal(execStub.args[1][0], 'pm2 save', 'should call save')
-  })
+test('pm2RestartAndSave should throw error if restart fails', async (t) => {
+  const { execStub } = t.nr
+  const err = new Error('restart failed')
+  execStub.onCall(0).rejects(err)
+  await assert.rejects(pm2RestartandSave(1), err, 'should reject with restart error')
+})
 
-  test.test('pm2RestartAndSave should throw error if restart fails', (t) => {
-    const err = new Error('restart failed')
-    execStub.onCall(0).rejects(err)
-    t.rejects(pm2RestartandSave(1), err, 'should reject with restart error')
-    t.end()
-  })
-
-  test.test('pm2RestartAndSave should throw error if save fails', async (t) => {
-    const err = new Error('save failed')
-    execStub.onCall(0).resolves()
-    execStub.onCall(1).rejects(err)
-    t.rejects(pm2RestartandSave(1), err, 'should reject with save error')
-    t.end()
-  })
+test('pm2RestartAndSave should throw error if save fails', async (t) => {
+  const { execStub } = t.nr
+  const err = new Error('save failed')
+  execStub.onCall(0).resolves()
+  execStub.onCall(1).rejects(err)
+  await assert.rejects(pm2RestartandSave(1), err, 'should reject with save error')
 })
